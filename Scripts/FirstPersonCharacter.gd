181240abb9e755302ef684320267bcd1c562e9c9
Smg065 <tails2tails@gmail.com>
2025-08-09 01:28:08 -0600

    Push Before Sleep

--- Scripts/FirstPersonCharacter.gd
+++ Scripts/FirstPersonCharacter.gd
@@ -3,21 +3,38 @@ class_name Player
 
 @export var mainCamera : Camera3D
 @export var cuteCamera : Camera3D
+@export var mouseSensitivity : float = .005
 
 func _ready() -> void:
 	sync_cameras()
 
-func _physics_process(delta: float) -> void:
-	var inputVector : Vector2 = get_inputs()
-	var fromCamera : Vector3 = Vector3(inputVector.x, 0, inputVector.y)
-	velocity = fromCamera * delta * 100
-	print(fromCamera * delta * 10)
+func _process(delta: float) -> void:
+	var inputVector : Vector2 = get_move_vector()
+	var relativeMove : Vector3 = global_transform.basis.z * inputVector.y
+	relativeMove += global_transform.basis.x * inputVector.x
+	velocity = relativeMove * delta * 150
+	move_and_slide()
+	if Input.is_action_just_pressed("Pause"):
+		if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		else:
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	move_camera(-Input.get_vector("CameraLeft","CameraRight","CameraUp","CameraDown") * Vector2(10,7))
 
-func get_inputs() -> Vector2:
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		clamp(mainCamera.rotation_degrees.x, -120, 120)
+		move_camera(-event.relative)
+
+func get_move_vector() -> Vector2:
 	var inputVector : Vector2
-	inputVector.x = Input.get_axis("MoveBack", "MoveForward")
-	inputVector.y = Input.get_axis("MoveLeft", "MoveRight")
+	inputVector.y = Input.get_axis("MoveForward", "MoveBack")
+	inputVector.x = Input.get_axis("MoveLeft", "MoveRight")
 	return inputVector.normalized()
+
+func move_camera(cameraInput : Vector2) -> void:
+	rotate_y(cameraInput.x * mouseSensitivity)
+	mainCamera.rotate_x(cameraInput.y * mouseSensitivity)
 
 func sync_cameras():
 	cuteCamera.position = mainCamera.position
