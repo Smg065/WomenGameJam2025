d6331e7fdd0ba73ca62662c638a518b715c01c7a
Smg065 <tails2tails@gmail.com>
2025-08-09 13:29:13 -0600

    Enemy Pathing Working

--- Scripts/NPC.gd
+++ Scripts/NPC.gd
@@ -6,9 +6,21 @@ class_name NPC
 @export var navInfo : NavigationAgent3D
 @export var player : Player
 
-func _process(delta: float) -> void:
+@export var speed : float = 1.5
+
+func _physics_process(delta: float) -> void:
+	
+	chase_logic(delta)
+
+func chase_logic(delta: float) -> void:
 	navInfo.target_position = player.global_position
+	navInfo.get_next_path_position()
 	var pathNodes : PackedVector3Array = navInfo.get_current_navigation_path()
-	#for eachNode in pathNodes.size():
-	#	eachNode
-	linear_velocity = navInfo.get_velocity()
+	var distToTravel = delta * speed
+	for eachNode in pathNodes.size() - 1:
+		var distThisPath : float = pathNodes[eachNode].distance_to(pathNodes[eachNode + 1])
+		if distThisPath >= distToTravel:
+			var offset : Vector3 = pathNodes[eachNode].direction_to(pathNodes[eachNode + 1]) * distToTravel
+			position += offset
+			break
+		distToTravel -= distThisPath
